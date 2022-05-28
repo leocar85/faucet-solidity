fb6f743928baa7bd931b68c044f062171ba1b9f7
leocardoso-legion <leonardoc@bluetrailsoft.com>
2022-05-28 14:22:35 -0300

    Metamask integration

--- src/App.js
+++ src/App.js
@@ -1,8 +1,75 @@
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider'
+
+import { loadContract } from './utils/load-contracts';
+import './App.css';
+
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+    contract: null
+  })
+
+  const [account, setAccount] = useState(null)
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+      //const contract = await loadContract('Faucet', provider);
+      const artifact = await loadContract('Faucet');
+      console.log(artifact)
+
+      if (provider) {
+        setWeb3Api({
+          provider,
+          web3: new Web3(provider),
+          //contract
+        })
+      } else {
+        console.error('Please, install Metamask.')
+      }
+    }
+    loadProvider();
+  }, [])
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0])
+    }
+
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3])
+
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="is-flex is-align-items-center">
+            <span className='mr-2'>
+              <strong>Account: </strong>
+            </span>
+            <h1>
+              { account ? 
+                  account : 
+                  <button 
+                    className="button is-small"
+                    onClick={() => web3Api.provider.request({method: "eth_requestAccounts"})}
+                  >
+                      Connect Wallet
+                  </button>}
+            </h1>
+          </div>
+          <div className="balance-view is-size-2 my-4">
+            Current Balance <strong>10</strong> ETH
+          </div>
+          <button className="button is-link mr-2">Donate</button>
+          <button className="button is-primary">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
